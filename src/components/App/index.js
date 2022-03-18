adb8234b1ffcd96ce239bb0ab7f3f724b362a022
RomainBoudet <romainoclock@gmail.com>
2022-03-18 10:57:55 +0100

    add comment for install

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,6 @@
+// Ce composant est un container à composants, dans lequel je mettrais tous mes autres composants.
+// Et cet unique composant App sera rendu via la méthode render dans le fichier index.js
+// situé a la racine du fichier src.
 // == Import npm
 import React from 'react';
 
